5d4964d0f7680f39a3924bc344cb782056c89e98
Prashant Chauhan <prashant.chauhan@appdirect.com>
2021-08-13 12:09:24 +0530

    Initial commit

---
+++ azure-plugin-mock-utility/mock-utility/src/main/java/com/appdirect/iaas/azure/mockutility/MockUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.appdirect.iaas.azure.mockutility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockUtilityApplication.class, args);
+	}
+
+}
