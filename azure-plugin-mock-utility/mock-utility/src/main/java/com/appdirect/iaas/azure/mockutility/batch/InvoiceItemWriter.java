1b6f23bb431b61ba702f86933bff99a374bdf511
Prashant Chauhan <prashant.chauhan@appdirect.com>
2021-08-19 20:11:08 +0530

    Code refactoring

--- azure-plugin-mock-utility/mock-utility/src/main/java/com/appdirect/iaas/azure/mockutility/batch/InvoiceItemWriter.java
+++ azure-plugin-mock-utility/mock-utility/src/main/java/com/appdirect/iaas/azure/mockutility/batch/InvoiceItemWriter.java
@@ -2,6 +2,7 @@ package com.appdirect.iaas.azure.mockutility.batch;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -40,6 +41,18 @@ public class InvoiceItemWriter implements ItemWriter<InvoiceLineItem> {
 
     private static final String selfResourceLinkURITemplate = "/invoices/${invoiceId}/lineitems?provider=OneTime&invoicelineitemtype=${usageType}&size=${size}";
     private static final String nextResourceLinkURITemplate = "/invoices/${invoiceId}/lineitems?provider=OneTime&invoicelineitemtype=${usageType}&size=${size}&seekOperation=Next";
+    public static final String USAGE_TYPE_DAILY = "usagelineitems";
+    public static final String USAGE_TYPE_ONE_TIME = "billinglineitems";
+    public static final String ONE_TIME_JSON_RESPONSE_FILE = "OneTimeResponse_";
+    public static final String DAILY_RATED_JSON_REPONSE_FILE = "DailyRated_";
+    public static final String JSON_FILE_EXTENTION = ".json";
+    public static final String MS_CONTINUATION_TOKEN = "MS-ContinuationToken";
+    public static final String HTTP_METHOD_GET = "GET";
+    public static final String NEXT_RESOURCE_LINK = "next";
+    public static final String SELF_RESOURCE_LINK = "self";
+    public static final String INVOICE_ID_TOKEN = "invoiceId";
+    public static final String SIZE_TOKEN = "size";
+    public static final String USAGE_TYPE_TOKEN = "usageType";
 
     private static Long lineItemsToWrite;
     private static int oneTimeJsonFileCount = 1;
@@ -50,10 +63,10 @@ public class InvoiceItemWriter implements ItemWriter<InvoiceLineItem> {
 
     @Value("${mock.numberOfLineItems}")
     private Long numberOfLineItems;
-    
+
     @Value("${apiResponsePath.OneTime}")
     private String oneTimeResponsePath;
-    
+
     @Value("${apiResponsePath.DailyRated}")
     private String dailyRatedResponsePath;
 
@@ -83,29 +96,15 @@ public class InvoiceItemWriter implements ItemWriter<InvoiceLineItem> {
             oneTimeInvoiceLineItemResponse.setList(oneTimeInvoiceLineItemBeans);
             oneTimeInvoiceLineItemResponse.setTotalCount(oneTimeInvoiceLineItemBeans.size());
 
-            if (!isLastResponse) {
-                continuationToken = RandomStringUtils.random(200, true, true);
-            }
+            continuationToken = generateContinuationToken(isLastResponse);
             oneTimeInvoiceLineItemResponse.setContinuationToken(continuationToken);
-            oneTimeInvoiceLineItemResponse.setLinks(getLinks(isLastResponse, continuationToken, "billinglineitems", ((OneTimeInvoiceLineItem) invoiceLineItem).getInvoiceNumber(), String.valueOf(items.size())));
-            
-            String jsonFilePathName = oneTimeResponsePath.concat("/OneTimeResponse_").concat(String.valueOf(oneTimeJsonFileCount++)).concat(".json");
+            oneTimeInvoiceLineItemResponse.setLinks(getLinks(isLastResponse, continuationToken, USAGE_TYPE_ONE_TIME, ((OneTimeInvoiceLineItem) invoiceLineItem).getInvoiceNumber(), String.valueOf(items.size())));
+
+            String jsonFilePathName = oneTimeResponsePath.concat("/").concat(ONE_TIME_JSON_RESPONSE_FILE).concat(String.valueOf(oneTimeJsonFileCount++)).concat(".json");
             String jsonResponse = gson.toJson(oneTimeInvoiceLineItemResponse);
-            
-            BufferedWriter bufferedWriter = null;
-            try {
-                FileWriter writer = new FileWriter(jsonFilePathName);
-                bufferedWriter = new BufferedWriter(writer);
-                bufferedWriter.write(jsonResponse);
-                bufferedWriter.close();
-            } catch (Exception e) {
-                throw e;
-            } finally {
-                if (bufferedWriter != null) {
-                    bufferedWriter.close();
-                }
-            }
-            
+
+            writeResponseToJsonFile(jsonFilePathName, jsonResponse);
+
         } else {
 
             DailyRatedUsageItemsResponse dailyRatedUsageItemsResponse = new DailyRatedUsageItemsResponse();
@@ -116,67 +115,85 @@ public class InvoiceItemWriter implements ItemWriter<InvoiceLineItem> {
             dailyRatedUsageItemsResponse.setList(dailyRatedUsageLineItemBeans);
             dailyRatedUsageItemsResponse.setTotalCount(dailyRatedUsageLineItemBeans.size());
 
-            if (!isLastResponse) {
-                continuationToken = RandomStringUtils.random(200, true, true);
-            }
+            continuationToken = generateContinuationToken(isLastResponse);
 
             dailyRatedUsageItemsResponse.setContinuationToken(continuationToken);
-            dailyRatedUsageItemsResponse.setLinks(getLinks(isLastResponse, continuationToken, "usagelineitems", ((DailyRatedUsageLineItem) invoiceLineItem).getInvoiceNumber(), String.valueOf(items.size())));
+            dailyRatedUsageItemsResponse.setLinks(getLinks(isLastResponse, continuationToken, USAGE_TYPE_DAILY, ((DailyRatedUsageLineItem) invoiceLineItem).getInvoiceNumber(), String.valueOf(items.size())));
 
-            String jsonFilePathName = dailyRatedResponsePath.concat("/DailyRated_").concat(String.valueOf(dailyRatedJsonFileCount++)).concat(".json");
+            String jsonFilePathName = dailyRatedResponsePath.concat("/").concat(DAILY_RATED_JSON_REPONSE_FILE).concat(String.valueOf(dailyRatedJsonFileCount++)).concat(JSON_FILE_EXTENTION);
             String jsonResponse = gson.toJson(dailyRatedUsageItemsResponse);
-            
-            BufferedWriter bufferedWriter = null;
-            try {
-                FileWriter writer = new FileWriter(jsonFilePathName);
-                bufferedWriter = new BufferedWriter(writer);
-                bufferedWriter.write(jsonResponse);
-                bufferedWriter.close();
-            } catch (Exception e) {
-                throw e;
-            } finally {
-                if (bufferedWriter != null) {
-                    bufferedWriter.close();
-                }
-            }
+
+            writeResponseToJsonFile(jsonFilePathName, jsonResponse);
         }
 
         lineItemsToWrite -= items.size();
     }
 
+    private String generateContinuationToken(boolean isLastResponse) {
+        String continuationToken = null;
+        if (!isLastResponse) {
+            continuationToken = RandomStringUtils.random(200, true, true);
+        }
+        return continuationToken;
+    }
+
+    private void writeResponseToJsonFile(String jsonFilePathName, String jsonResponse) throws IOException {
+        BufferedWriter bufferedWriter = null;
+        try {
+            FileWriter writer = new FileWriter(jsonFilePathName);
+            bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write(jsonResponse);
+            bufferedWriter.close();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+        }
+    }
+
     private Map<String, ResourceLink> getLinks(boolean isLastResponse, String continuationToken, String usageType, String invoiceId, String pageSize) {
         Map<String, ResourceLink> links = new HashMap<>();
         Map<String, String> templateTokens = new HashMap<>();
-        templateTokens.put("invoiceId", invoiceId);
-        templateTokens.put("size", pageSize);
-        templateTokens.put("usageType", usageType);
+        templateTokens.put(INVOICE_ID_TOKEN, invoiceId);
+        templateTokens.put(SIZE_TOKEN, pageSize);
+        templateTokens.put(USAGE_TYPE_TOKEN, usageType);
 
         StringSubstitutor stringSubstitutor = new StringSubstitutor(templateTokens);
 
         if (!isLastResponse) {
-            ResourceLink nextLink = new ResourceLink();
-            List<ResourceLinkHeader> headers = new ArrayList<>();
-            ResourceLinkHeader nextLinkHeader = new ResourceLinkHeader();
-            nextLinkHeader.setKey("MS-ContinuationToken");
-            nextLinkHeader.setValue(continuationToken);
-            headers.add(nextLinkHeader);
-            nextLink.setHeaders(headers);
-            nextLink.setMethod("GET");
-
-            nextLink.setUri(stringSubstitutor.replace(nextResourceLinkURITemplate));
-
-            links.put("next", nextLink);
+            links.put(NEXT_RESOURCE_LINK,  generateNextResourceLink(continuationToken, stringSubstitutor));
         }
 
+        links.put(SELF_RESOURCE_LINK, generateSelfLink(stringSubstitutor));
+
+        return links;
+    }
+
+    private ResourceLink generateSelfLink(StringSubstitutor stringSubstitutor) {
         ResourceLink selfLink = new ResourceLink();
         List<ResourceLinkHeader> headers = Collections.emptyList();
 
         selfLink.setHeaders(headers);
         selfLink.setUri(stringSubstitutor.replace(selfResourceLinkURITemplate));
-        selfLink.setMethod("GET");
+        selfLink.setMethod(HTTP_METHOD_GET);
+        return selfLink;
+    }
 
-        links.put("self", selfLink);
+    private ResourceLink generateNextResourceLink(String continuationToken, StringSubstitutor stringSubstitutor) {
+        ResourceLink nextLink = new ResourceLink();
 
-        return links;
+        ResourceLinkHeader nextLinkHeader = new ResourceLinkHeader();
+        nextLinkHeader.setKey(MS_CONTINUATION_TOKEN);
+        nextLinkHeader.setValue(continuationToken);
+
+        List<ResourceLinkHeader> headers = new ArrayList<>();
+        headers.add(nextLinkHeader);
+        nextLink.setHeaders(headers);
+        nextLink.setMethod(HTTP_METHOD_GET);
+
+        nextLink.setUri(stringSubstitutor.replace(nextResourceLinkURITemplate));
+        return nextLink;
     }
 }
